db7383ab87076beb8afb8f460f83defa51f786e4
ayequill <ayequillgh@gmail.com>
2022-11-19 18:07:45 +0000

    initial files commit

---
+++ index.js
@@ -0,0 +1,36 @@
+let homeScoreBoardText = document.getElementById ("home-score")
+let guestScoreBoardText = document.getElementById ("guest-score")
+let homeScore = 0
+let guestScore = 0
+
+function homeAdd1 (){
+    homeScore ++
+    homeScoreBoardText.textContent = homeScore
+}
+
+function homeAdd2 () {
+    homeScore += 2
+    homeScoreBoardText.textContent = homeScore
+}
+
+function homeAdd3 () {
+    homeScore += 3
+    homeScoreBoardText.textContent = homeScore
+}
+
+
+function guestAdd1 (){
+    guestScore ++
+    guestScoreBoardText.textContent = guestScore
+}
+
+function guestAdd2 () {
+    guestScore += 2
+    guestScoreBoardText.textContent = guestScore
+}
+
+function guestAdd3 () {
+    guestScore += 3
+    guestScoreBoardText.textContent = guestScore
+}
+
